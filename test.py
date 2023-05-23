af6d6400cae63b390ee75d1606234a0624065ed7
PaniaguaF <98367564+PaniaguaF@users.noreply.github.com>
2023-05-22 20:53:14 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestGuessingGame(unittest.TestCase):
+    def test_getCode(self):
+        self.assertEqual(len(getCode()), 4)
+        self.assertRaises(Exception, getCode, 10)
+
+    def test_compareCode(self):
+        self.assertEqual(compareCode([1, 2, 3, 4], [1, 2, 3, 4]), True)
+        self.assertEqual(compareCode([1, 2, 3, 4], [4, 3, 2, 1]), False)
+
+if __name__ == '__main__':
+    unittest.main()
